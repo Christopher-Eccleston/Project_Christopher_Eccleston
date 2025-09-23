50cc97dbe6d182ab8d4cfa298c06fc74c8d7e8fe
[Christopher Eccleston] <[caeccleston2001@gmail.com]>
2025-09-23 00:46:15 -0400

    Csv file and papaparser added

--- Javascript/Week1.js
+++ Javascript/Week1.js
@@ -4,3 +4,25 @@ function ShowHelloMessage() {
 }
 
 document.getElementById("mybutton").onclick = ShowHelloMessage;
+
+const fileUrl = '/Database/Data.csv'
+
+Papa.parse(fileUrl, {
+    download:true,
+    header: true,
+    complete: function(results) {
+        console.log("Parsed data:", results.data);
+
+        const data = results.data;
+        if (data.length > 0) {
+            const outputElement = document.getElementById('output');
+            if (outputElement) {
+                const firstRow = data[0];
+                outputElement.textContent = `First product is: ${firstRow.name}`;
+            }
+        }
+    },
+    error: function(err, file) {
+        console.error("Error parsing file:", err);
+    }
+});
